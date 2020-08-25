a712fb234360fcb6ac40a11445a0fdefdbe03cbd
hsqoo <913650332abc@gmail.com>
2020-08-25 18:09:55 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,57 @@
 import axios from 'axios'
 import qs from 'qs'
 
-const baseUrl = "";
+const baseUrl = "http://localhost:3000";
 
 function callApi (endpoint, postParam, method) {
-    let fullUrl = (endpoint.indexOf(baseUrl) === -1) ? baseUrl + endpoint : endpoint;
-    let parseParams = qs.stringify(postParam);
+    // let fullUrl = (endpoint.indexOf(baseUrl) === -1) ? baseUrl + endpoint : endpoint;
+    let cookie = window.localStorage.getItem('myMusicCookies');
+    let fullUrl = baseUrl + endpoint;
+    let timeStramp = + new Date();
+    let parseParams;
+    parseParams = Object.assign(postParam,{timeStramp},{cookie});
+    console.log(postParam, parseParams)
+
+    parseParams = qs.stringify(postParam);
     return new Promise((resolve, reject) => {
         if (method === 'GET'){
-            fullUrl += '&' + parseParams;
+            fullUrl += '?'
+            fullUrl +=  parseParams + '&';
             axios.get(fullUrl)
                 .then(res=> {
-                    if(res.statusCode ==200){
+                    if(res.status ==200){
                         resolve(res)
                     }else {
-                        console.log(res)
+                        // console.log(res)
                         reject(res)
                     }
                 }).catch(err=> {
-                    console.log(err)
+                    console.log('axios error', err)
                     reject(err)
                 });
+        } else {
+            axios({
+                method: method && method.toLowerCase() || 'post',
+                url: fullUrl,
+                data: postParam,
+                success: ()=>{
+
+                }
+            }).then(res=> {
+                if(res.status ==200){
+                    resolve(res)
+                }else {
+                    console.log(res)
+                    reject(res)
+                }
+            }).catch(err=> {
+                console.log('axios error', err)
+                reject(err)
+            });
         }
-    
+
     })
 }
 
-export const login = (postParam) => callApi(`/login/cellphone`, postParam);
+export const login = (postParam) => callApi(`/login/cellphone`, postParam, 'POST');
+export const checkLoginStatus = (postParam) => callApi(`/login/status`, postParam, 'GET');
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,129 +2,63 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Router, Route, Switch } from "react-router";
 import { Form, Input, Button, Checkbox } from 'antd';
-import {login} from '../../api';
+import {login, checkLoginStatus} from '../../api';
 import './home.scss'
 
-const That = this;
-const layout = {
-    labelCol: {
-        span: 8,
-    },
-    wrapperCol: {
-        span: 16,
-    },
-};
-const tailLayout = {
-    wrapperCol: {
-        offset: 8,
-        span: 16,
-    },
-};
-const LoginForm = () => {
-    const onFinish = values => {
-        console.log('Success:', values);
-    };
-
-    const onFinishFailed = errorInfo => {
-        console.log('Failed:', errorInfo);
-    };
-    // bug 未完结
-    // function handlePhoneChange(event) {
-    //     console.log(this)
-    //     That.state({
-    //         phoneNumber: event.target.value
-    //     });
-    // }
-    // function handlePwdChange(event) {
-    //     console.log(this)
-    //     this.state({
-    //         password: event.target.value
-    //     });
-    // }
-    function handleLoginEvent(event) {
-        login({'phone': this.phoneNumber, 'password': this.password}).then((res)=> {
-            console.log(res);
-        })
-
-    }
-     // bug 未完结
-    return (
-        <Form
-        {...layout}
-        name="basic"
-        initialValues={{
-            remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        >
-        <Form.Item
-            label="Username"
-            name="username"
-            rules={[
-            {
-                required: true,
-                message: 'Please input your username!',
-            },
-            ]}
-        >
-            <Input onChange={this.handlePhoneChange}/>
-        </Form.Item>
-
-        <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-            {
-                required: true,
-                message: 'Please input your password!',
-            },
-            ]}
-        >
-            <Input.Password onChange={this.handlePwdChange}/>
-        </Form.Item>
-
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-            <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit" onClick={handleLoginEvent}>
-            Submit
-            </Button>
-        </Form.Item>
-        </Form>
-    );
-};
 
 class Home extends React.Component{
-    state = {
-        phoneNumber: '',
-        password: '',
-    }
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         phoneNumber: '',
-    //         password: '',
-    //     }
+    // state = {
+    //     phoneNumber: '',
+    //     password: '',
     // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            phoneNumber: '',
+            password: '',
+        }
+        this.handlePhoneChange = this.handlePhoneChange.bind(this);
+        this.handlePwdChange = this.handlePwdChange.bind(this);
+        this.handleClickEvent = this.handleClickEvent.bind(this);
+    }
     handlePhoneChange(event) {
-        console.log(this)
-        That.state({
+        this.setState({
             phoneNumber: event.target.value
         });
     }
-    handlePwdChange() {
-
+    handlePwdChange(event) {
+        this.setState({
+            password: encodeURIComponent(event.target.value)
+        });
+    }
+    handleClickEvent() {
+        console.log(this);
+        login({phone: this.state.phoneNumber, password: this.state.password}).then((res)=>{
+            console.log(res.data.cookie);
+            if(res.status === 200 && res.data.cookie) {
+                window.localStorage.setItem('myMusicCookies', res.data.cookie)
+            }
+        }).then(()=> {
+            checkLoginStatus({})
+        })
     }
 	render(){
 		return(
 			<div className="login">
-                <LoginForm />
+                <div>
+                    <div className="row">
+                        <label>帐号：</label><input type="text" onChange={this.handlePhoneChange}/>
+                    </div>
+                    <div className="row">
+                        <label>密码：</label><input type="password" onChange={this.handlePwdChange}/>
+                    </div>
+                    <div className="row">
+                        <button onClick={this.handleClickEvent}>登录</button>
+                    </div>
+                </div>
 			</div>
 		);
 	}
 }
- 
+
 export default Home;
